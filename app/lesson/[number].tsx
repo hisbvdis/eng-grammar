0b6864b48fe18a56ce097c2d85668401bb5f8d7c
Roma Zvarych (hisbvdis) <hisbvdis@gmail.com>
2024-06-08 12:25:59 +0300

    fix

--- app/lesson/[number].tsx
+++ app/lesson/[number].tsx
@@ -3,21 +3,19 @@ import { Stack, useLocalSearchParams } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 // -----------------------------------------------------------------------------
 import type { Lesson, Lessons } from "@/assets/lessons";
-import { View, useWindowDimensions } from "react-native";
 import Container from "@/components/Container";
 
 
 export default function LessonsPage() {
   const params = useLocalSearchParams();
   const [ lesson, setLesson ] = useState<Lesson>();
-  const { width } = useWindowDimensions();
 
   useEffect(() => {(async () => {
-    const lessonsJson = await AsyncStorage.getItem("lessons");
-    if (!lessonsJson) return;
-    const lessonsArray = JSON.parse(lessonsJson) as Lessons;
-    const lessonObject = lessonsArray.find((lesson) => lesson.number === params.number);
-    setLesson(lessonObject);
+    const allLessonsJson = await AsyncStorage.getItem("lessons");
+    if (!allLessonsJson) return;
+    const allLessonsArray = JSON.parse(allLessonsJson) as Lessons;
+    const currentLessonObject = allLessonsArray.find((lesson) => lesson.number === params.number);
+    setLesson(currentLessonObject);
   })()}, [])
 
   return (
