f48d500c452cdeea0eb2f650fbf426c216bedeeb
Roma Zvarych (hisbvdis) <hisbvdis@gmail.com>
2024-06-10 15:09:57 +0300

    Simple theory added

--- app/quiz.tsx
+++ app/quiz.tsx
@@ -31,9 +31,9 @@ export default function quiz() {
       <PagerView style={{flex: 1}} onPageSelected={() => setIsShowAnswer(false)}>
         {lesson && lesson[type].map((item, i, arr) => (
           <View key={item.id}>
-            <Text style={{marginTop: "auto", fontSize: 30, textAlign: "center", marginBottom: 50}}>{item.question}</Text>
+            <Text style={{marginTop: "auto", fontSize: 20, textAlign: "center", marginBottom: 50}}>{item.question}</Text>
             {!isShowAnswer && <Btn onPress={() => setIsShowAnswer(true)}>Показать ответ</Btn>}
-            {isShowAnswer && <Text style={{fontSize: 30, textAlign: "center"}}>{item.answer}</Text>}
+            {isShowAnswer && <Text style={{fontSize: 20, textAlign: "center"}}>{item.answer}</Text>}
             <View style={{marginTop: "auto", flexDirection: "row", gap: 5, flexWrap: "wrap"}}>
               {arr.map((item, k) => <View style={[{borderRadius: 25, borderWidth: 1, flexBasis: 10, height: 5}, [k <= i && {backgroundColor: "black"}]]}></View>)}
             </View>
